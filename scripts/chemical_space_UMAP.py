4adf2e3d95dd260afafb0b680939a49cf4b7cee7
TomSmithCGAT <tss38@mrc-tox.cam.ac.uk>
2024-05-13 21:41:50 +0100

    updates UMAP and adds pollutants vs drug max Tanimoto plot

--- scripts/chemical_space_UMAP.py
+++ scripts/chemical_space_UMAP.py
@@ -33,7 +33,7 @@ def get_UMAP_vs_background(plotter_obj,
                            background_name='PubChem',
                            foreground_name=None,
                            n_neighbors = 20,
-                           min_dist = 0.25,
+                           min_dist = 0.2,
                            random_state=1985):
    
 	data = plotter_obj._Plotter__df_descriptors
@@ -71,7 +71,8 @@ def plot_UMAP_vs_background(df_data,
                                      "#000000",
                                      "#CC79A7",
                                      "#009E73"),
-                            outfile_prefix = None):
+                            outfile_prefix = None,
+                            highlight_compounds=None):
 	
 	size=20/2.54
 	hue = 'type'
@@ -93,9 +94,14 @@ def plot_UMAP_vs_background(df_data,
                                s=size*0.5,
                                rasterized=True)
 
+	if highlight_compounds:
+		highlight_df = df_data[df_data['CID'].isin(highlight_compounds)]
+		plt.scatter(highlight_df[0], highlight_df[1], marker='x',
+		            color='black', s=size*0.5, linewidths=0.5)
+
 	plot.set_label("scatter")
 	axis = plot
-	plot.legend(markerscale=size*0.05, fontsize=size, frameon=False)
+	plot.legend(markerscale=size*0.5, fontsize=size, frameon=False)
 	# Remove units from axis
 	axis.set(yticks=[])
 	axis.set(xticks=[])
@@ -114,6 +120,9 @@ def plot_UMAP_vs_background(df_data,
 	if(outfile_prefix):
 		plt.savefig(outfile_prefix + '.png', dpi=400)
 		plt.savefig(outfile_prefix + '.pdf', dpi=400)
+
+		if highlight_compounds:
+			highlight_df.to_csv(outfile_prefix + '_highlight_CIDs.tsv', index=False, sep='\t')
 
 
 def main(argv=None):
@@ -169,9 +178,16 @@ def main(argv=None):
 						type=float,
 						help=('UMAP Mimumum distance'))
 
+	parser.add_argument('--highlight-compounds',
+					dest='highlight_compounds',
+					type=float,
+					nargs='+',
+					default=None,
+					help=('CIDs for compounds to highlight'))
+
 
 	parser.set_defaults(n_neighbors=20,
-						min_dist=0.1,
+						min_dist=0.2,
 						seed=1985,
 						output_prefix='./umap')
 
@@ -218,11 +234,24 @@ def main(argv=None):
 
 	logging.info("UMAP")
 
-	umap = get_UMAP_vs_background(cp, n_neighbors=args.n_neighbors, min_dist=args.min_dist)
+	my_umap = get_UMAP_vs_background(cp,
+	                                 n_neighbors=args.n_neighbors,
+	                                 min_dist=args.min_dist,
+	                                 random_state=args.seed)
+	
+	my_umap = my_umap.reset_index()
+
+	if args.highlight_compounds is not None:
+		# Add CID if highlighting of specific CID is required
+		retained_smiles = set(cp._Plotter__df_descriptors.index)
+		cids = list(pubchem_smiles['CID']) + list(screen_smiles['PubChem CID'])
+		cids = [cid for cid, smiles in zip(cids, all_smiles) if smiles in retained_smiles]
+		my_umap['CID'] = cids
 
 	logging.info("Plotting")
 
-	plot_UMAP_vs_background(umap, outfile_prefix=args.output_prefix)
+	plot_UMAP_vs_background(my_umap, outfile_prefix=args.output_prefix,
+	                        highlight_compounds=args.highlight_compounds)
 
 	logging.info("Extracting ECFPs")
 	all_compounds2smiles = pd.DataFrame(all_targets, all_smiles, columns=['Compound Class'])
@@ -258,9 +287,48 @@ def main(argv=None):
 	min_jacc_df = pd.DataFrame({'min_jacc': min_jacc,
 	                           'comparison': comparison})
 
-	logging.info("Saving distances to file")		
+	logging.info("Saving minimum jaccard distances to file")		
 	min_jacc_df.to_csv(args.output_prefix + '_min_jaccard.tsv', index=False, sep='\t')
 	
+	logging.info("Get minimum distances between pollutants and drugs")	
+	
+	pollutant_rows = ecfp[ecfp['Compound Class'].isin(pollutant_classes)]
+	drug_rows = ecfp[ecfp['Compound Class']=='Pharmaceutical drugs']
+
+	pollutant_drug_jacc_dis = cdist(pollutant_rows.drop('Compound Class', axis=1),
+	                                drug_rows.drop('Compound Class', axis=1),
+	                                'jaccard')
+
+	min_pollutant_drug_jacc = np.min(pollutant_drug_jacc_dis, 1)
+	
+	# Dicts required to map from smile back to name and CID
+	smile2chem = {x:y for x,y in zip(screen_smiles['PUBCHEM_isosmiles'], screen_smiles['Chemical name'])}
+	smile2cid = {x:y for x,y in zip(screen_smiles['PUBCHEM_isosmiles'], screen_smiles['PubChem CID'])}
+
+	min_jac_pol_drug_rows = list()
+	
+	# Find the drug with the min jacc distance for each pollutant
+	for pol_ix, min_jacc in enumerate(min_pollutant_drug_jacc):
+	    pol_smile = pollutant_rows.index[pol_ix]
+	    
+	    # Will break ties since only first match returned
+	    min_ix = list(pollutant_drug_jacc_dis[pol_ix]).index(min_jacc)
+
+	    drug_smile = drug_rows.index[min_ix]
+	    
+	    min_jac_pol_drug_rows.append([smile2cid[pol_smile],
+	          smile2chem[pol_smile].upper(),
+	          smile2cid[drug_smile],
+	          smile2chem[drug_smile].upper(),
+	          1-min_jacc])
+
+	min_pollutant_drug_jacc_df = pd.DataFrame.from_records(min_jac_pol_drug_rows)
+	min_pollutant_drug_jacc_df.columns = [
+		'Pollutant_PubChem_CID', 'Pollutant_name', 'Drug_PubChem_CID', 'Drug_name', 'Tanimoto']
+	min_pollutant_drug_jacc_df.sort_values('Tanimoto', ascending=False, inplace=True)
+	
+	min_pollutant_drug_jacc_df.to_csv(args.output_prefix + '_min_pollutant_drug_jaccard.tsv', sep='\t')
+
 	logging.info("Finished!")
 
 if __name__ == "__main__":
--- scripts/plot_tanimoto.R
+++ scripts/plot_tanimoto.R
@@ -8,6 +8,8 @@ suppressPackageStartupMessages(library(optparse))
 option_list <- list( 
   make_option("--min-jacc-inf", type='character', dest='min_jacc_inf',
               help="Infile with minimum jaccard distances, as produced by chemical_space_UMAP.py"),
+  make_option("--min-jacc-pol-drug-inf", type='character', dest='min_jacc_pol_drug_inf',
+              help="Infile with minimum jaccard distances between pollutants and drugs, as produced by chemical_space_UMAP.py"),
   make_option("--plot-prefix", type='character', dest='plot_prefix',
               help="Prefix for plots"))
 
@@ -15,6 +17,28 @@ opt <- parse_args(OptionParser(option_list=option_list))
 
 # Read in the minimum jaccard distances
 jacc_dist <- read.delim(opt$min_jacc_inf)
+jacc_dist_pol_drug <- read.delim(opt$min_jacc_pol_drug_inf)
+
+p <- jacc_dist_pol_drug %>%
+  ggplot(aes(Tanimoto)) +
+  geom_histogram() +
+  geom_vline(xintercept=0.75, linetype=2, colour='grey') +
+  xlab('Pollutants vs Pharmaceutical drugs\nmaximum Tanimoto similarity') +
+  ylab('Count') +
+  scale_y_continuous(expand = c(0,0)) +
+  theme_bw(base_size =20) + 
+  theme(panel.grid = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_blank(), 
+        aspect.ratio = 1,
+        axis.line = element_line(colour = "black", linewidth = 0.5),
+        legend.position=c(1,1), legend.justification = c("right", "top"))
+
+ggsave(sprintf('%s_tanimoto_pollutant_vs_drug.png', opt$plot_prefix),
+       width=6, height=6, plot=p)
+ggsave(sprintf('%s_tanimoto_pollutant_vs_drug.pdf', opt$plot_prefix),
+       width=6, height=6, plot=p)
+
 
 # Plot the distances as an ECDF, with y-axis flipped
 p <- jacc_dist %>%
@@ -35,39 +59,39 @@ p <- jacc_dist %>%
         legend.position=c(1,1), legend.justification = c("right", "top"))
 
 
-  # from https://stackoverflow.com/questions/27233738/finding-location-of-maximum-d-statistic-from-ks-test
-  compare <- function(x, y) {
-    n <- length(x); m <- length(y)
-    w <- c(x, y)
-    o <- order(w)
-    z <- cumsum(ifelse(o <= n, m, -n))
-    i <- which.max(abs(z))
-    w[o[i]]
-  }
+# from https://stackoverflow.com/questions/27233738/finding-location-of-maximum-d-statistic-from-ks-test
+compare <- function(x, y) {
+  n <- length(x); m <- length(y)
+  w <- c(x, y)
+  o <- order(w)
+  z <- cumsum(ifelse(o <= n, m, -n))
+  i <- which.max(abs(z))
+  w[o[i]]
+}
 
-  all_jacc <- jacc_dist %>% filter(comparison=='All') %>% pull(min_jacc) * 100
-  drugs_jacc <- jacc_dist %>% filter(comparison=='Pharmaceutical drugs') %>% pull(min_jacc) * 100
+all_jacc <- jacc_dist %>% filter(comparison=='All') %>% pull(min_jacc) * 100
+drugs_jacc <- jacc_dist %>% filter(comparison=='Pharmaceutical drugs') %>% pull(min_jacc) * 100
 
-  u <- compare(all_jacc, drugs_jacc)
-  e.x <- ecdf(all_jacc)
-  e.y <- ecdf(drugs_jacc)
+u <- compare(all_jacc, drugs_jacc)
+e.x <- ecdf(all_jacc)
+e.y <- ecdf(drugs_jacc)
 
-  D_yend = 100*(e.y(u))
-  D_ystart = 100*(e.x(u))
-  D_x = 1-(u/100)
+D_yend = 100*(e.y(u))
+D_ystart = 100*(e.x(u))
+D_x = 1-(u/100)
 
-  ks_res <- ks.test(all_jacc, drugs_jacc)
+ks_res <- ks.test(all_jacc, drugs_jacc)
 
-  p1 <- p + geom_segment(x=D_x, xend=D_x, y=D_ystart, yend=D_yend,
-                         colour="#d55e00", linewidth=0.25) +
-    annotate(geom='text', label=sprintf('KS-test:\nD = %s\np%s',
-                                        round(ks_res$statistic,3),
-                                        ifelse(ks_res$p.value>2.2e-16, paste0(' = ', ks_res$p.value), ' < 2.2e-16')),
-             x=0.5,
-             y=mean(c(D_ystart, D_yend)),
-             vjust=0.5, hjust=0,
-             colour="#d55e00",
-             size=5)
+p1 <- p + geom_segment(x=D_x, xend=D_x, y=D_ystart, yend=D_yend,
+                       colour="#d55e00", linewidth=0.25) +
+  annotate(geom='text', label=sprintf('KS-test:\nD = %s\np%s',
+                                      round(ks_res$statistic,3),
+                                      ifelse(ks_res$p.value>2.2e-16, paste0(' = ', ks_res$p.value), ' < 2.2e-16')),
+           x=0.5,
+           y=mean(c(D_ystart, D_yend)),
+           vjust=0.5, hjust=0,
+           colour="#d55e00",
+           size=5)
 
 ggsave(sprintf('%s_tanimoto_background_vs_screen.png', opt$plot_prefix),
        width=6, height=6, plot=p1)
